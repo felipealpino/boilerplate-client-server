28bcd6d88ab880f84f193070f9c061b3d1aa558c
Felipe Gontijo Alpino <felipe@sunday.market>
2024-05-20 21:53:37 -0300

    melhorias na arquiterura

--- client/src/modules/users/types.ts
+++ client/src/modules/users/types.ts
@@ -1,8 +1,3 @@
 import { UseQueryOptions } from '@tanstack/react-query';
 
 export type CommonArgs = { options?: UseQueryOptions<any> };
-
-export interface User {
-  id: number;
-  name: string;
-}
